fd937ac08495234f9f89b52fa5497b427036d56d
liujiahao <edinliu@gmail.com>
2020-09-23 10:44:56 +0800

    成功存成json

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 console.clear()
 console.log('===========================================================')
 const getUserID = require('./getUserID')
+const fs = require('fs')
 
-const allUserIdArray = []
-getUserID()
+
+getUserID().then(allUid =>
+  fs.writeFile('userIds.json', JSON.stringify(allUid), (err) => {
+    if (err) throw err;
+  })
+)
 
 
 // fs.writeFile(filename, data, options, callback)
